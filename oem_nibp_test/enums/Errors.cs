a584865bb75d497d899827dbd28c79ffe7c9c896
Evgenii Molchkov <evg.molt@gmail.com>
2022-08-25 16:10:44 +0400

    Add Errors enum

---
+++ oem_nibp_test/enums/Errors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oem_nibp_test.enums
+{
+    internal enum Errors
+    {
+        None,
+        ZeroPressureCalibration,
+        AirNotPump,
+        TooHighDecompressionSpeed,
+        PulseNotFound,
+        PulsationTooSmall,
+        TooLowDecompressionSpeed,
+        TooManyArtifacts,
+        SystolicTooHigh,
+        DiastolicTooLow,
+        PressureTooHigh,
+        TooLongTime,
+        WrongCuff
+    }
+}
